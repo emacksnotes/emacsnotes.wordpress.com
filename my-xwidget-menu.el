eb92b737f1ec4a685a7d9b8a1cd3b3e83a48c14f
Emacksnotes <emacksnotes@gmail.com>
2018-08-17 19:10:17 +0530

    my-xwidget-menu.el: New

---
+++ my-xwidget-menu.el
@@ -0,0 +1,50 @@
+(define-key-after global-map
+  [menu-bar tools xwidget-webkit-browse-url]
+  `(menu-item "Browse with XWidget Webkit ..." xwidget-webkit-browse-url :enable
+	      (featurep 'xwidget-internal)
+	      :help ,(documentation 'xwidget-webkit-browse-url))
+  'browse-web)
+
+(with-eval-after-load '"xwidget"
+  (when
+      (featurep 'xwidget-internal)
+    (easy-menu-define my-xwidget-menu xwidget-webkit-mode-map "Menu for Xwidget Webkit."
+      '("Xwidget"
+	["Browse Url" xwidget-webkit-browse-url :help "Ask xwidget-webkit to browse URL"]
+	["Reload" xwidget-webkit-reload :help "Reload current url"]
+	["Back" xwidget-webkit-back :help "Go back in history"]
+	"--"
+	["Insert String" xwidget-webkit-insert-string :help "current webkit widget"]
+	["End Edit Textarea" xwidget-webkit-end-edit-textarea :help "End editing of a webkit text area"]
+	"--"
+	["Scroll Forward" xwidget-webkit-scroll-forward :help "Scroll webkit forwards"]
+	["Scroll Backward" xwidget-webkit-scroll-backward :help "Scroll webkit backwards"]
+	"--"
+	["Scroll Up" xwidget-webkit-scroll-up :help "Scroll webkit up"]
+	["Scroll Down" xwidget-webkit-scroll-down :help "Scroll webkit down"]
+	"--"
+	["Scroll Top" xwidget-webkit-scroll-top :help "Scroll webkit to the very top"]
+	["Scroll Bottom" xwidget-webkit-scroll-bottom :help "Scroll webkit to the very bottom"]
+	"--"
+	["Zoom In" xwidget-webkit-zoom-in :help "Increase webkit view zoom factor"]
+	["Zoom Out" xwidget-webkit-zoom-out :help "Decrease webkit view zoom factor"]
+	"--"
+	["Fit Width" xwidget-webkit-fit-width :help "Adjust width of webkit to window width"]
+	["Adjust Size" xwidget-webkit-adjust-size :help "Manually set webkit size to width W, height H"]
+	["Adjust Size Dispatch" xwidget-webkit-adjust-size-dispatch :help "Adjust size according to mode"]
+	["Adjust Size To Content" xwidget-webkit-adjust-size-to-content :help "Adjust webkit to content size"]
+	"--"
+	["Copy Selection As Kill" xwidget-webkit-copy-selection-as-kill :help "Get the webkit selection and put it on the kill-ring"]
+	["Current Url" xwidget-webkit-current-url :help "Get the webkit url and place it on the kill-ring"]
+	"--"
+	["Show Element" xwidget-webkit-show-element :help "Make webkit xwidget XW show a named element ELEMENT-SELECTOR"]
+	["Show Id Element" xwidget-webkit-show-id-element :help "Make webkit xwidget XW show an id-element ELEMENT-ID"]
+	["Show Id Or Named Element" xwidget-webkit-show-id-or-named-element :help "Make webkit xwidget XW show a name or element id ELEMENT-ID"]
+	["Show Named Element" xwidget-webkit-show-named-element :help "Make webkit xwidget XW show a named element ELEMENT-NAME"]
+	"--"
+	["Cleanup" xwidget-cleanup :help "Delete zombie xwidgets"]
+	["Event Handler" xwidget-event-handler :help "Receive xwidget event"]
+	"--"
+	["Xwidget Webkit Mode" xwidget-webkit-mode :style toggle :selected xwidget-webkit-mode :help "Xwidget webkit view mode"]))))
+
+(provide 'my-xwidget-menu)
